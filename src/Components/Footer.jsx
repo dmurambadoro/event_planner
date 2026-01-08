7babf8ba0b64b3ffa44a978a56d3b8db406b2861
dmurambadoro <dkmurambadoro@gmail.com>
2026-01-08 00:02:52 -0500

    add footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <div>
+                    <h2>We strive to be number one in customer service</h2>
+                    <p>&copy; Event Planner Organization. All rights reserved.</p>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
